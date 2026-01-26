d8400ac410270b7a450272d52c280bdfb01189c5
dk-dev01 <derick.hatakeyama@gmail.com>
2026-01-26 14:37:14 -0300

    colocando todos arquivos css dentro da pasta 'css'

--- etapa-2.php
+++ etapa-2.php
@@ -77,7 +77,7 @@ if (isset($_POST["botao"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style-etapa2.css">
+    <link rel="stylesheet" href="css/style-etapa2.css">
     <title>Biblioteca tech</title>
 </head>
 <body>
